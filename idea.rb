6c208dadade8c0ec3273802b585847753d3726cc
Meeka <rrgayhart@gmail.com>
2013-10-22 21:06:45 -0600

    finishes strike through and some general code cleanup

--- idea.rb
+++ idea.rb
@@ -12,6 +12,18 @@ class Idea
   property :rank, Integer
   property :completed_at, DateTime
 
+  def completed?
+    true if completed_at
+  end
+
+  def self.completed
+    all(:completed_at.not => nil)
+  end
+
+  def link
+    "<a href=\"idea/#{self.id}\">#{self.title}</a>" 
+  end
+
   def like!
     @rank ||= 0
     @rank += 1
