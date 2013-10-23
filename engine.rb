f62140e459290852fe94e5025e3b9b875ad173e6
Meeka <rrgayhart@gmail.com>
2013-10-23 08:40:03 -0600

    Splits out engine and adds test file

--- engine.rb
+++ engine.rb
@@ -2,3 +2,55 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require_relative 'idea'
+
+post '/idea/create' do
+  idea = Idea.new(:title => params[:title], :description => params[:description])
+  if idea.save
+    status 201
+    redirect '/idea/'+idea.id.to_s
+  else
+    status 412
+    redirect '/ideas'
+  end
+end
+
+get '/idea/:id' do
+  @idea = Idea.get(params[:id])
+  erb :edit
+end
+
+get '/' do
+  @ideas = Idea.all
+  erb :index
+end
+
+
+get '/idea/:id/edit' do
+  @idea = Idea.get(params[:id])
+  erb :edit
+end
+
+
+put '/idea/:id' do
+  idea = Idea.get(params[:id])
+  idea.completed_at = params[:completed] ?  Time.now : nil
+  idea.title = (params[:title])
+  idea.description = (params[:description])
+  if idea.save
+    status 201
+    redirect '/idea/'+idea.id.to_s
+  else
+    status 412
+    redirect '/ideas'   
+  end
+end
+
+get '/idea/:id/delete' do
+  @idea = Idea.get(params[:id])
+  erb :delete
+end
+
+delete '/idea/:id' do
+  Idea.get(params[:id]).destroy
+  redirect '/ideas'  
+end
--- idea.rb
+++ idea.rb
@@ -25,7 +25,7 @@ class Idea
   end
 
   def like!
-    @rank ||= 0
+    @rank ||= 0    
     @rank += 1
   end
 
@@ -33,59 +33,6 @@ class Idea
     other.rank <=> rank
   end
 end
-
-post '/idea/create' do
-  idea = Idea.new(:title => params[:title], :description => params[:description])
-  if idea.save
-    status 201
-    redirect '/idea/'+idea.id.to_s
-  else
-    status 412
-    redirect '/ideas'
-  end
-end
-
-get '/idea/:id' do
-  @idea = Idea.get(params[:id])
-  erb :edit
-end
-
-get '/' do
-  @ideas = Idea.all
-  erb :index
-end
-
-
-get '/idea/:id/edit' do
-  @idea = Idea.get(params[:id])
-  erb :edit
-end
-
-
-put '/idea/:id' do
-  idea = Idea.get(params[:id])
-  idea.completed_at = params[:completed] ?  Time.now : nil
-  idea.title = (params[:title])
-  idea.description = (params[:description])
-  if idea.save
-    status 201
-    redirect '/idea/'+idea.id.to_s
-  else
-    status 412
-    redirect '/ideas'   
-  end
-end
-
-get '/idea/:id/delete' do
-  @idea = Idea.get(params[:id])
-  erb :delete
-end
-
-delete '/idea/:id' do
-  Idea.get(params[:id]).destroy
-  redirect '/ideas'  
-end
-
 
 
 DataMapper.auto_upgrade!
